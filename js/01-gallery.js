4195fa2ac7ebf6d1d551df76d8a894ea3bbed583
AntonPetrenko <paa2017sh@gmail.com>
2023-02-12 00:05:06 +0200

    task-01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,16 +24,7 @@ function makeGallery(items) {
     })
     
 }
-/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
+
 const items = makeGallery(galleryItems);
 divGalleryRef.append(...items);
 
